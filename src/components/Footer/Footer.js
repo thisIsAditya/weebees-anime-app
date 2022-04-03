430adf30dbab7b2b0e774f7ff3e340aeb5744887
Aditya Pandey <pandey.api2k@gmail.com>
2022-04-03 16:13:12 +0530

    Typo Fixes, added Gihub Icon and set global css rule for links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,6 +4,8 @@ import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import fevicon from "../../assets/favicon.ico"; 
 const FooterComponent = () => {
+
+
     return ( 
         <div>
             <div className="bg-dark bg-opacity-75 p-4">
@@ -24,8 +26,14 @@ const FooterComponent = () => {
                                         <h3 className="font-heading">About Us</h3>
                                     </Card.Title>
                                     <Card.Text>                                
-                                        WeeBees Anime is a demo website made using React and Bootstrap. I contains, Corousel, Slider and Infinite scrolling grid components.
+                                        WeeBees Anime is a demo website made using React and Bootstrap. It contains Corousel, Slider and Infinite Scrolling Grid components.
+                                        Feel free to make this website more awesome by contributing to it on Github.<br />
                                     </Card.Text>
+                                    <a href="https://github.com/thisIsAditya/weebees-anime-app" target="_blank" rel="noreferrer">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                                            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+                                        </svg>
+                                    </a>
                                 </Card.Body>
                             </Card>
                         </Col>                               
@@ -35,8 +43,7 @@ const FooterComponent = () => {
                                     <Card.Title>
                                         <h3 className="font-heading">Contact Us</h3>
                                     </Card.Title>                                                                 
-                                    <div> 
-                                        <h4 className="mb-3">You can contact us at:</h4>
+                                    <div className="d-flex justify-content-center align-items-center my-5">                                         
                                         <a href="https://www.linkedin.com/in/pandeyapi" className="m-2" target="_blank" rel="noreferrer">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
   <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
@@ -61,7 +68,11 @@ const FooterComponent = () => {
                 </Container>
             </div>
             <footer className="bg-dark d-flex justify-content-center">
-                <p className="text-white mt-2 font-weight-bold">Made with Love by Aditya Pandey</p>
+                <p className="text-white mt-2 font-weight-bold">Made with love {
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+                  </svg>
+                } by <a href="https://www.linkedin.com/in/pandeyapi" target="_blank" rel="noreferrer" >Aditya Pandey</a></p>
             </footer>
         </div>
      );
